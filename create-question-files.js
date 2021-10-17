61f60eaea75d09fa3ca84e9a0270bc29049918ce
David VanDusen <github@dvw3.com>
2021-10-17 10:32:44 -0700

    Styles and bugfixes

--- create-question-files.js
+++ create-question-files.js
@@ -3,5 +3,7 @@ const fs = require('fs');
 const questions = fs.readFileSync(process.argv[2], 'utf8').split('\n');
 
 for (var i = 0; i < questions.length; i++) {
-  fs.writeFileSync(`public/questions/${i}.txt`, questions[i]);
+  if (questions[i]) {
+    fs.writeFileSync(`public/questions/${i}.txt`, questions[i]);
+  }
 }
